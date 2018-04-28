7adc1d2c73dbae6ed98c2142fff125eb21c0b97a
nobodylovesm3 <inc0n3ck@abv.bg>
2018-04-28 12:16:27 +0300

    google maps and second activity added

--- app/src/main/java/inc0n3ck/hackaton/MainActivity.java
+++ app/src/main/java/inc0n3ck/hackaton/MainActivity.java
@@ -1,7 +1,10 @@
 package inc0n3ck.hackaton;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button secondActivity = (Button) findViewById(R.id.mapActivityBtn);
+        secondActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(startIntent);
+            }
+        });
+
     }
 
 }
