c3632b7512da41718d32e903817bd39ecef30357
Silverlined <dimitriy.georgiev@gmail.com>
2018-04-28 12:57:04 +0300

    FullScreen + added Gif

--- app/src/main/java/inc0n3ck/hackaton/MainActivity.java
+++ app/src/main/java/inc0n3ck/hackaton/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,15 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button secondActivity = (Button) findViewById(R.id.mapActivityBtn);
-        secondActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent startIntent = new Intent(getApplicationContext(), MapsActivity.class);
-                startActivity(startIntent);
-            }
-        });
 
+        ImageView hubbleView = (ImageView) findViewById(R.id.gifView);
+        hubbleView.setOnClickListener(v -> goToSecondActivity());
     }
 
+    private void setFullScreen() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+        );
+    }
+
+    private void goToSecondActivity() {
+        Intent startIntent = new Intent(getApplicationContext(), DestinationSwipe.class);
+        startActivity(startIntent);
+    }
 }
+
